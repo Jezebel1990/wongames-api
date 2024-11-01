a4eb89325f48eccbeed23618e4a2b16b84ff3f8f
Jezebel1990 <jezebelguedes@gmail.com>
2024-11-01 00:18:20 -0300

    Customizing the strapi

---
+++ src/admin/app.tsx
@@ -0,0 +1,42 @@
+import Icon from './extensions/icon.png'
+import Logo from './extensions/logo.svg'
+
+export default {
+  config: {
+    auth: {
+      logo: Logo,
+    },
+    head: {
+      favicon: Icon,
+    },
+    locales: [],
+    translations: {
+      en: {
+        "Auth.form.welcome.title": "Welcome to Won Games!",
+        "Auth.form.welcome.subtitle": "Log in to your account",
+        "app.components.LeftMenu.navbrand.title": "Won Games Dashboard",
+        "app.components.HomePage.welcomeBlock.content.again": "Welcome to Won Games Admin! Add more games, categories and publishers from Content Manager.",
+      },
+    },
+    menu: {
+      logo: Icon,
+    },
+    theme: {
+      light: {},
+      dark: {
+        colors: {
+          primary100: "#030415",
+          primary600: "#53F9FF",
+          primary700: "#53F9FF",
+          neutral0: "#0d102f",
+          neutral100: "#030415",
+        },
+      },
+    },
+    tutorials: false,
+    notifications: {
+      releases: false,
+    }
+  },
+  bootstrap() {},
+};
